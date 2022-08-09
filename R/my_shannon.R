b8d314fccbd1937db8dd682cc11dc694a24daaa4
tawnunes <taw.ynunes@gmail.com>
2022-08-09 16:45:09 -0300

    Initial commit

---
+++ R/my_shannon.R
@@ -0,0 +1,19 @@
+#' Shannon Diversity Index
+#'
+#' Custom function to calculate shannon diversity index
+#'
+#' @param x A vector with values of abundance in the community
+#'
+#' @return A numeric vector of length 1 with the value of the Shannon diversity
+#' @export
+#'
+#' @examples
+#' com <- c(1, 2, 3)
+#' my_shannon(com)
+
+my_shannon <- function(x) {
+ x <- x[x > 0]
+ sum_all <- sum(x)
+ pi <- x/sum_all
+ -sum(pi*log(pi))
+}
